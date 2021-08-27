21a396d6e194e2acde9894731bc8e0fa27743499
steph durino <steph.durino@gmail.com>
2021-08-27 13:19:32 -0400

    oops i broke it.

--- client/components/SingleLevel.js
+++ client/components/SingleLevel.js
@@ -1,69 +1,75 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { DocCSS, DocHTML, DocIMPORTS, DocMocha } from './docSections'
-import level, { fetchLevel } from '../store/level';
+import React from 'react';
+import { connect } from 'react-redux';
+import { DocCSS, DocHTML, DocIMPORTS, DocMocha } from './docSections';
+import level, { fetchLevel, nextLevel } from '../store/level';
+import { Link } from 'react-router-dom';
 import Editor from '@monaco-editor/react';
 import Anime from 'react-anime';
 
 class SingleLevel extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			js: '',
+			doc: '',
+			scale: 0,
+			animationIsDone: false,
+			testResults: [],
+		};
+		this.onChange = this.onChange.bind(this);
+		this.setDoc = this.setDoc.bind(this);
+		this.setAnimationDone = this.setAnimationDone.bind(this);
+		this.handleNextLevel = this.handleNextLevel.bind(this);
+	}
 
+	async componentDidMount() {
+		setTimeout(this.setAnimationDone, 6300);
+		const userHasAccesToLevel = await this.props.getLevel(
+			this.props.match.params.id
+		);
 
-    constructor() {
-        super()
-        this.state = {
-          js: '',
-          doc: '',
-          scale: 0,
-          animationIsDone: false,
-          testResults: []
-        }
-        this.onChange = this.onChange.bind(this)
-        this.setDoc = this.setDoc.bind(this)
-        this.setAnimationDone = this.setAnimationDone.bind(this)
-      }
+		if (userHasAccesToLevel) {
+			this.setDoc(true);
+		} else {
+			this.props.history.push('/level/unauthorized');
+		}
 
-      async componentDidMount() {
-          setTimeout(this.setAnimationDone, 6300);
-          const userHasAccesToLevel = await this.props.getLevel(this.props.match.params.id)
+		// Create IE + others compatible event handler
+		var eventMethod = window.addEventListener
+			? 'addEventListener'
+			: 'attachEvent';
+		var eventer = window[eventMethod];
+		var messageEvent = eventMethod == 'attachEvent' ? 'onmessage' : 'message';
+		function eventFunction(e) {
+			if (typeof e.data === 'string') {
+				this.setState({ testResults: JSON.parse(e.data) });
+			}
+		}
 
-          if (userHasAccesToLevel){
-            this.setDoc(true)
-          } else{
-            this.props.history.push("/level/unauthorized")
-          }
+		// Listen to message from child window
+		eventer(messageEvent, eventFunction.bind(this), false);
+	}
 
-          // Create IE + others compatible event handler
-          var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-          var eventer = window[eventMethod];
-          var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-          function eventFunction(e) {
-            if (typeof e.data === 'string') {
-              this.setState({testResults: JSON.parse(e.data)})
-            }
-          }
+	setAnimationDone() {
+		this.setState({
+			animationIsDone: true,
+		});
+	}
 
-          // Listen to message from child window
-          eventer(messageEvent,eventFunction.bind(this),false);
+	onChange(newValue) {
+		this.setState({
+			js: newValue,
+		});
+	}
 
+	async handleNextLevel() {
+		await this.props.newLevel(this.props.level.id);
+		this.props.history.push(`/level/${this.props.level.id + 1}`);
+		console.log(`/level/${level.id + 1}?password=${level.password}`);
+	}
 
-
-      }
-
-      setAnimationDone() {
-        this.setState({
-          animationIsDone: true
-        })
-      }
-
-      onChange(newValue) {
-        this.setState({
-          js: newValue
-        })
-      }
-
-
-      setDoc(canDo) {
-        const doc = `
+	setDoc(canDo) {
+		const doc = `
         <html>
           <body>
           ${DocHTML(this.props.level)}
@@ -78,96 +84,132 @@ class SingleLevel extends React.Component {
 
           </script>
         </html>
-      `
-        if (canDo) {
-          let scale = this.state.scale
-          if (scale >= 6) {
-            scale = 6
-          } else {
-            scale += 1
-          }
-          this.setState({
-            doc,
-            scale
-          })
-        }
+      `;
+		if (canDo) {
+			let scale = this.state.scale;
+			if (scale >= 6) {
+				scale = 6;
+			} else {
+				scale += 1;
+			}
+			this.setState({
+				doc,
+				scale,
+			});
+		}
+	}
 
+	render() {
+		console.log('TEST RESULTS:', this.state.testResults);
+		const sampleCode = this.props.level.startingJS;
+		const level = this.props.level;
+		return this.props.level.startingJS ? (
+			<Anime duration={6000} opacity={[0, 1]}>
+				<div id='level'>
+					{this.props.level.id && (
+						<div>
+							<Anime
+								duration={3000}
+								translateX={[-1000, -500]}
+								easing={'linear'}>
+								<img src='../generalJoe.png' className='generalJoe'></img>
+							</Anime>
+							<h2>{this.props.level.name}</h2>
+						</div>
+					)}
+					{/* <h3>Welcome, {username}</h3> */}
+					<div id='codeIframe'>
+						<iframe
+							id='thing'
+							srcDoc={this.state.doc}
+							title='output'
+							sandbox='allow-scripts allow-top-navigation allow-same-origin'
+							frameBorder='0'
+							width='100%'
+							height='100%'
+						/>
+					</div>
 
-      }
-
-    render () {
-      console.log("TEST RESULTS:", this.state.testResults)
-      const sampleCode = this.props.level.startingJS
-      const level = this.props.level
-        return (
-          this.props.level.startingJS ?
-          <Anime duration={6000} opacity={[0,1]}>
-          <div id="level">
-            {(this.props.level.id) && <div>
-                  <Anime duration={3000} translateX={[-1000,-500]} easing={'linear'}>
-                    <img src="../generalJoe.png" className="generalJoe"></img>
-                  </Anime>
-                  <h2>{this.props.level.name}</h2>
-                  </div>}
-            {/* <h3>Welcome, {username}</h3> */}
-            <div id="codeIframe">
-              <iframe id="thing"
-                srcDoc={this.state.doc}
-                title="output"
-                sandbox="allow-scripts allow-top-navigation allow-same-origin"
-                frameBorder="0"
-                width="100%"
-                height="100%"
-              />
-            </div>
-
-
-              <button className="runButton" onClick={() => this.setDoc(this.state.animationIsDone)}>Run</button>
-            <div className="duckDiv">
-            {level.tests && level.tests.map((test, index) => {
-              if (this.state.testResults[index] === 'FAILED') {
-                return (<Anime duration={3000} rotate={[0,-20,0,20]} scale={[this.state.scale]} loop={true} direction={'alternate'}>
-                  <img src="../theEvilRubberDuck.png" className="evilDuck" ></img>
-                </Anime>)
-              } else {
-                return (<Anime duration={3000} rotate={[0,20,0,-20]}  loop={true} direction={'alternate'}>
-                  <img src="../theDocileRubberDuck.jpg" className="goodDuck" ></img>
-                </Anime>)
-              }
-              })}
-            </div>
-
-            <Editor
-              height="50vh"
-              width="75vw"
-              fontsize="12px"
-              value={this.state.js}
-              defaultLanguage="javascript"
-              theme="vs-dark"
-              onChange={this.onChange}
-              defaultValue={sampleCode}
-              options={{
-                readOnly: false,
-                lineHeight: 25,
-              }}
-            />
-          </div>
-          </Anime> : null
-        )
-      }
-
-
-
+					<button
+						className='runButton'
+						onClick={() => this.setDoc(this.state.animationIsDone)}>
+						Run
+					</button>
+					<div className='duckDiv'>
+						{level.tests &&
+							level.tests.map((test, index) => {
+								if (this.state.testResults[index] === 'FAILED') {
+									return (
+										<Anime
+											duration={3000}
+											rotate={[0, -20, 0, 20]}
+											scale={[this.state.scale]}
+											loop={true}
+											direction={'alternate'}>
+											<img
+												src='../theEvilRubberDuck.png'
+												className='evilDuck'></img>
+										</Anime>
+									);
+								} else {
+									return (
+										<Anime
+											duration={3000}
+											rotate={[0, 20, 0, -20]}
+											loop={true}
+											direction={'alternate'}>
+											<img
+												src='../theDocileRubberDuck.jpg'
+												className='goodDuck'></img>
+										</Anime>
+									);
+								}
+							})}
+					</div>
+					<div id='popbox'>
+						{level.tests &&
+							level.tests.every((test, index) => {
+								if (this.state.testResults[index] === 'PASSED') {
+									return true;
+								}
+							}) && (
+								<div id='popup'>
+									<i id='fa fa-check'></i>
+									<div id='success'>Success!!</div>
+									<div id='popup-desc'>You did it!</div>
+									<button className='next-level' onClick={this.handleNextLevel}>
+										Go to the next Level!
+									</button>
+								</div>
+							)}
+					</div>
+					<Editor
+						height='50vh'
+						width='75vw'
+						fontsize='12px'
+						value={this.state.js}
+						defaultLanguage='javascript'
+						theme='vs-dark'
+						onChange={this.onChange}
+						defaultValue={sampleCode}
+						options={{
+							readOnly: false,
+							lineHeight: 25,
+						}}
+					/>
+				</div>
+			</Anime>
+		) : null;
+	}
 }
 
+const mapState = (state) => ({
+	level: state.level,
+});
 
-const mapState = state => ({
-    level: state.level
-})
+const mapDispatch = (dispatch, { history }) => ({
+	getLevel: (id) => dispatch(fetchLevel(id)),
+	newLevel: (id) => dispatch(nextLevel(id, history)),
+});
 
-const mapDispatch = dispatch => ({
-    getLevel: (id) => dispatch(fetchLevel(id))
-})
-
-
-export default connect(mapState, mapDispatch)(SingleLevel)
+export default connect(mapState, mapDispatch)(SingleLevel);
--- server/api/levels.js
+++ server/api/levels.js
@@ -1,26 +1,45 @@
-const router = require('express').Router()
-const { models: { Level, Test, User }} = require('../db')
+const router = require('express').Router();
+const {
+	models: { Level, Test, User },
+} = require('../db');
 const { requireToken, userHasLevel } = require('./gatekeepingMiddleware');
-module.exports = router
-
-
-
+module.exports = router;
 
 router.get('/list', requireToken, async (req, res, next) => {
-  try {
-    const levels = await Level.findAll({include: {model: User, where: {id: req.user.id}, required: false}, order: [['id','ASC']]})
-    res.send(levels)
-  } catch (err) {
-    next(err)
-  }
-})
+	try {
+		const levels = await Level.findAll({
+			include: { model: User, where: { id: req.user.id }, required: false },
+			order: [['id', 'ASC']],
+		});
+		res.send(levels);
+	} catch (err) {
+		next(err);
+	}
+});
 
+router.get('/complete/:id', requireToken, async (req, res, next) => {
+	try {
+		const nextLvl = await req.user.hasLevel(parseInt(req.params.id));
+		console.log('this is next level', nextLvl);
+		if (!nextLvl) {
+			await req.user.addLevel(parseInt(req.params.id));
+		}
+		const level = await Level.findByPk(parseInt(req.params.id) + 1, {
+			include: Test,
+		});
+		if (level) {
+			res.json(level);
+		}
+	} catch (err) {
+		next(err);
+	}
+});
 
 router.get('/:id', requireToken, userHasLevel, async (req, res, next) => {
-  try {
-    const level = await Level.findByPk(req.params.id,{include: Test})
-    res.json(level)
-  } catch (err) {
-    next(err)
-  }
-})
+	try {
+		const level = await Level.findByPk(req.params.id, { include: Test });
+		res.json(level);
+	} catch (err) {
+		next(err);
+	}
+});
