7c428cded5faa0868aab7538bde954ad569439b4
Andrew Kerr <ak373@cornell.edu>
2021-08-27 16:31:23 -0400

    signed out and signed in pages prettified

--- client/components/Home.js
+++ client/components/Home.js
@@ -8,9 +8,8 @@ export class Home extends React.Component {
 
   render() {
     return (
-      <Anime duration={6000} opacity={[0,1]}>
-      <div style={{display: 'flex'}}>
-        {this.props.username && ('Welcome, ' + this.props.username)}
+      <Anime duration={5000} opacity={[0,1]}>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
         <Anime duration={3500} rotate={[0,-20,0,20,0]} loop={true} >
           <img src="theEvilRubberDuck.png" className="evilDuck"></img>
         </Anime>
@@ -27,10 +26,27 @@ export class Home extends React.Component {
           <img src="theEvilRubberDuck.png" className="evilDuck"></img>
         </Anime>
       </div>
+      <div style={{display: 'flex', justifyContent: "center"}}>
+        <Anime duration={5000} translateX={[-700,-50]} easing={'linear'}>
+        {!this.props.username && <img src="../generalJoe.png" className="openingOfficer"></img>}
+        </Anime>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <h2 style={{textAlign: "center", color: "#333D29"}}>{this.props.username && ('Welcome, ' + this.props.username + '.')}</h2>
+          <h3 style={{textAlign: "center", color: "#333D29"}}>{this.props.username && (<Anime delay={1000} duration={1300} opacity={[0,1]} >
+            <>Are you ready for your next mission?</>
+          </Anime>)}</h3>
+          <h3 style={{textAlign: "center", color: "#333D29"}}>{!this.props.username && ('LEARN TO CODE')}</h3>
+          <h3 style={{textAlign: "center", color: "#333D29"}}>{!this.props.username && ('JOIN THE FIGHT')}</h3>
+          <h3 style={{textAlign: "center", color: "#333D29"}}>{!this.props.username && ('DEFEAT THE EVIL DUCK ARMY!!')}</h3>
+        </div>
+        <Anime duration={5000} translateX={[700,50]} easing={'linear'}>
+        {!this.props.username && <img src="../sargeantSarah.png" className="openingOfficer"></img>}
+        </Anime>
+      </div>
       </Anime>
     )
   }
-  
+
 }
 
 const mapState = state => {
